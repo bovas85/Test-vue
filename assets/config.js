aff542fafae227f658cd0c18eae66f894bde0a50
Alessandro Giordo <bovas85@gmail.com>
2018-03-22 17:45:56 +0000

    added config

---
+++ assets/config.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Config = {
+  appTitle: "Vodafone",
+  appTitleShort: "VOF",
+  appDescription:
+    "Stay Connected With Vodafone's Most Reliable Network Ever. Brands: Apple, Samsung, Sony, Huawei, LG, BlackBerry, Motorola. Types: Pay Monthly Handsets, Pay As You Go Handsets, SIM Only, Mobile Broadband, Accessories.",
+  appThemeColor: "#ffffff ",
+  appBgColor: "#e60000 ",
+  appIcon: "/logo.png",
+  client: "assets/",
+  api: {
+    data: "phones.json"
+  }
+};
+
+module.exports = Config;
